bd22be375f958d57c7bd4d07a0d639048ce32197
leehyunjun97 <monsterstamp79@gmail.com>
2024-04-27 02:30:24 +0900

    액세스 토큰 만료시 리프레시 토큰으로 재발급 및 미들웨어 리다이렉팅

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getUserInfo } from './services/user/user';
-// import { signAccessToken } from './app/lib/jwt';
+import { signJWT } from './app/lib/jwt';
 
 export async function middleware(request: NextRequest) {
   // if (request.nextUrl.pathname.startsWith('/about')) {
@@ -9,30 +9,30 @@ export async function middleware(request: NextRequest) {
 
   let accessToken = request.cookies.get('accessToken');
 
-  if (!accessToken) return NextResponse.redirect(new URL('/', request.url));
-
   // 액세스 토큰이 없을 시 (혹은 만료가 되었을 시)
-  // if (!accessToken) {
-  //   let refreshToken = request.cookies.get('refreshToken');
+  if (!accessToken) {
+    let refreshToken = request.cookies.get('refreshToken');
 
-  //   // 리프레시 토큰도 없을 시 로그인(홈)으로 리다이렉팅
-  //   if (!refreshToken) return NextResponse.redirect(new URL('/', request.url));
+    // 리프레시 토큰도 없을 시 로그인(홈)으로 리다이렉팅
+    if (!refreshToken) return NextResponse.redirect(new URL('/', request.url));
 
-  //   const user = await getUserInfo(refreshToken.value);
-  //   const newAccessToken = signAccessToken(user);
+    // 재발급
+    const user = await getUserInfo(refreshToken.value);
+    const newAccessToken = (await signJWT(user)).accessToken;
 
-  //   console.log('새로운 액세스 토큰 발급: ', newAccessToken);
+    const response = NextResponse.redirect(new URL('/todos', request.url));
+    response.cookies.set('accessToken', newAccessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60,
+      path: '/',
+    });
+    return response;
+  }
 
-  //   return NextResponse.redirect(new URL('/todos', request.url));
-  // }
-
-  const user = await getUserInfo(accessToken.value);
-
-  console.log(user);
-
+  // const user = await getUserInfo(accessToken.value);
   return NextResponse.redirect(new URL('/todos', request.url));
 }
 
 export const config = {
-  matcher: ['/test/:path*'],
+  matcher: ['/todos/:path*'],
 };
